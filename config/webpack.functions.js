95664e9ddc90a02c6ea919dc7371454d75f82f64
Vivekanand <vivekanand.senthil@gmail.com>
2021-04-28 12:35:04 +0530

    Add files via upload

--- config/webpack.functions.js
+++ config/webpack.functions.js
@@ -1,1 +1,6 @@
+// https://www.frankcalise.com/netlify-functions-and-firebase-sdk-gotchas
+const nodeExternals = require("webpack-node-externals");
 
+module.exports = {
+  externals: [nodeExternals()],
+};
