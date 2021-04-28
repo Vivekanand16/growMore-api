f5852031fda28bbfa6d8208838c4d50145a4fad2
Vivekanand <vivekanand.senthil@gmail.com>
2021-04-28 12:33:39 +0530

    Add files via upload

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import admin from "firebase-admin";
+import { serviceAccount } from "./config";
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+const auth = admin.auth();
+const db = admin.firestore();
+
+export { auth, db };
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,1 +1,1 @@
-
+export * from "./firebase";
